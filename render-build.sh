c1a02983dba960ee48095a20b3ae0ff66e83415d
Abdualzizsm <Abdualzizsma@gmail.com>
2025-03-01 08:22:30 +0300

    تحديث الكود ليستخدم مكتبة OpenAI مباشرة مع توكن GitHub

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # استبدال متغيرات البيئة في index.html
-sed -i "s|__API_KEY__|$API_KEY|g" index.html
-sed -i "s|__ENDPOINT__|$ENDPOINT|g" index.html
-sed -i "s|__DEPLOYMENT_NAME__|$DEPLOYMENT_NAME|g" index.html
+sed -i "s|__API_KEY__|$GITHUB_TOKEN|g" index.html
 
 echo "تم استبدال متغيرات البيئة بنجاح"
